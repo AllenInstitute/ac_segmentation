7946abf4169756445ef3bf8c0891e87aeee0a389
Connor Laughland <71669181+conochur@users.noreply.github.com>
2025-04-22 08:09:26 -0700

    added min integer for lut_preprocess array function

--- src/ac_segmentation/utils/preprocess.py
+++ src/ac_segmentation/utils/preprocess.py
@@ -11,13 +11,17 @@ def lut_preprocess_array(arr, max_int):
     
     
     
-def lut_preprocess_array(arr, max_int):
+def lut_preprocess_array(arr, min_int=None, max_int=None):
     dtype =  str(arr.dtype)
     arr_max = int(arr.max())
     if not max_int:
         max_int=arr_max
     max_int=int(max_int)
     lut = np.empty(int(arr_max + max_int), dtype="uint8")
+    if min_int:
+        lut[:min_int] = 0
     lut[max_int:] = 255
     lut[:max_int] = np.round(np.arange(max_int) * (255 / max_int))
-    return lut[arr.astype('int16')].astype(dtype)
+    if 'int' not in str(arr.dtype):
+        arr = arr.astype('int16')
+    return lut[arr].astype(dtype)
